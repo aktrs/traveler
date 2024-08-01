1774ff75f664e88153ecc2fa8d210f8fabbf48dd
aktrs <174441696+aktrs@users.noreply.github.com>
2024-08-01 21:39:58 +0900

    マイページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only:[:index, :show]
+  resources :users, only:[:index, :show, :edit, :update]
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
