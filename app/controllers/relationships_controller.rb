40fdca523ec99948fe31678c3c486b0a101f6745
aktrs <174441696+aktrs@users.noreply.github.com>
2024-09-10 20:20:58 +0900

    [update]DM

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,6 @@
 class RelationshipsController < ApplicationController
     def create
-        user = User.find(params[:user_id])
+        user = User.find(params[:followed_id])
         current_user.follow(user.id)
         redirect_to request.referer
     end
